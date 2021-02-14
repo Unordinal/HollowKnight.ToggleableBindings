59c74550de246af7e2e1f6a434b0e0d5c5d6d057
Unordinal <unordinal@gmail.com>
2021-02-13 23:33:37 -0800

    New test mod that adds a new binding from an outside assembly.

---
+++ TestNewBinding/SpeedBinding.cs
@@ -0,0 +1,37 @@
+﻿#nullable enable
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+using ToggleableBindings;
+using UnityEngine;
+
+namespace TestNewBinding
+{
+    public sealed class SpeedBinding : Binding
+    {
+        [NotNull]
+        private Rigidbody2D? _rb2d;
+
+        public SpeedBinding() : base(nameof(SpeedBinding))
+        {
+
+        }
+
+        protected override void OnApplied()
+        {
+            _rb2d = HeroController.instance.GetComponent<Rigidbody2D>();
+            On.HeroController.Move += HeroController_Move;
+        }
+
+        protected override void OnRestored()
+        {
+            On.HeroController.Move -= HeroController_Move;
+        }
+
+        private void HeroController_Move(On.HeroController.orig_Move orig, HeroController self, float move_direction)
+        {
+            orig(self, move_direction);
+            _rb2d.velocity = new Vector2(_rb2d.velocity.x * 0.5f, _rb2d.velocity.y);
+        }
+    }
+}
---
+++ TestNewBinding/TestNewBinding.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using InControl;
+using Modding;
+using ToggleableBindings;
+using Vasi;
+
+namespace TestNewBinding
+{
+    public class TestNewBinding : Mod, ITogglableMod
+    {
+        public override string GetVersion() => VersionUtil.GetVersion<TestNewBinding>();
+
+        public override int LoadPriority() => 20;
+
+        public override void Initialize()
+        {
+            BindingManager.RegisterBinding<SpeedBinding>();
+        }
+
+        public void Unload()
+        {
+
+        }
+    }
+}
