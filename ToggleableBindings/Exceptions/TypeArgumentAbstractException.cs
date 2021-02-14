03d1096621a3d31173b21827e7d297b0d459d08f
Unordinal <unordinal@gmail.com>
2021-02-13 23:32:01 -0800

    Added exception type for type arguments so we can differentiate them for things such as abstract types or other such invalid situations.

---
+++ ToggleableBindings/Exceptions/TypeArgumentAbstractException.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace ToggleableBindings.Exceptions
+{
+    /// <summary>
+    /// The exception that is thrown when one of the type arguments provided to a method refers to a type that is abstract.
+    /// </summary>
+    public class TypeArgumentAbstractException : TypeArgumentException
+    {
+        private const string DefaultMessage = "The specified type parameter should not reference an abstract type.";
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TypeArgumentAbstractException"/> class.
+        /// </summary>
+        public TypeArgumentAbstractException() : this(DefaultMessage) { }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TypeArgumentAbstractException"/> class
+        /// with a specified error message.
+        /// </summary>
+        public TypeArgumentAbstractException(string message) : base(message) { }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TypeArgumentAbstractException"/> class
+        /// with a specified error message and the name of the type parameter that causes this exception.
+        /// </summary>
+        public TypeArgumentAbstractException(string message, string typeParamName) : base(message, typeParamName) { }
+
+        /// /// <summary>
+        /// Initializes a new instance of the <see cref="TypeArgumentAbstractException"/> class
+        /// with a specified error message and
+        /// a reference to the inner exception that is the cause of this exception.
+        /// </summary>
+        public TypeArgumentAbstractException(string message, Exception innerException) : base(message, innerException) { }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TypeArgumentAbstractException"/> class
+        /// with serialized data.
+        /// </summary>
+        /// <inheritdoc/>
+        protected TypeArgumentAbstractException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
+}
---
+++ ToggleableBindings/Exceptions/TypeArgumentException.cs
@@ -0,0 +1,67 @@
+﻿#nullable enable
+
+using System;
+using System.Runtime.Serialization;
+
+namespace ToggleableBindings.Exceptions
+{
+    /// <summary>
+    /// The exception that is thrown when one of the type arguments provided to a method is not valid.
+    /// </summary>
+    public class TypeArgumentException : Exception
+    {
+        private const string DefaultMessage = "An invalid type argument was specified.";
+        private const string TypeParamName_Name = "(Type parameter '{0}')";
+
+        public override string Message
+        {
+            get
+            {
+                string output = base.Message;
+                if (!string.IsNullOrEmpty(TypeParamName))
+                    output += " " + string.Format(TypeParamName_Name, TypeParamName);
+
+                return output;
+            }
+        }
+
+        /// <summary>
+        /// Gets the name of the type parameter that caused this exception.
+        /// </summary>
+        public string? TypeParamName { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TypeArgumentException"/> class.
+        /// </summary>
+        public TypeArgumentException() : this(DefaultMessage) { }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TypeArgumentException"/> class
+        /// with a specified error message.
+        /// </summary>
+        public TypeArgumentException(string? message) : base(message) { }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TypeArgumentException"/> class
+        /// with a specified error message and the name of the type parameter that causes this exception.
+        /// </summary>
+        public TypeArgumentException(string? message, string? typeParamName) : base(message)
+        {
+            TypeParamName = typeParamName;
+        }
+
+        /// /// <summary>
+        /// Initializes a new instance of the <see cref="TypeArgumentException"/> class
+        /// with a specified error message and
+        /// a reference to the inner exception that is the cause of this exception.
+        /// </summary>
+        public TypeArgumentException(string? message, Exception innerException) : base(message, innerException) { }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TypeArgumentException"/> class
+        /// with serialized data.
+        /// </summary>
+        /// <inheritdoc/>
+        protected TypeArgumentException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
+}
