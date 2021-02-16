a06b784a77f0a1f9001d4959f70b7da653712919
Unordinal <unordinal@gmail.com>
2021-02-15 17:40:21 -0800

    Made Binding serialization an opt-in and added an ID property for easier uniqueness (even though Name would've probably worked fine despite it beng a display name).

--- ToggleableBindings/Binding.cs
+++ ToggleableBindings/Binding.cs
@@ -10,31 +10,47 @@ namespace ToggleableBindings
 {
     /// <summary>
     /// Represents a binding - a modifier that changes the difficulty of the game when active.
+    /// <para>
+    /// The members of a binding are serialized to save settings on an opt-in basis. If you want a field
+    /// or property to be saved and loaded with a save file, mark it with the
+    /// <see cref="JsonPropertyAttribute"/>.
+    /// </para>
     /// </summary>
-    [JsonObject]
+    [JsonObject(MemberSerialization.OptIn)]
     public abstract class Binding
     {
         private const string MustBeNearBench = "Must be near a bench to {0} this binding."; // {0} = 'apply', 'restore'
 
+        /// <summary>
+        /// Invoked when this binding is applied.
+        /// </summary>
         public event BindingEventHandler? Applied;
 
+        /// <summary>
+        /// Invoked when this binding is restored.
+        /// </summary>
         public event BindingEventHandler? Restored;
 
         /// <summary>
-        /// Gets the name of this binding.
+        /// Gets the ID of this binding. Output is of the format '<c>Assembly::BindingTypeName</c>'.
+        /// <para/>
+        /// Example: <c>ToggleableBindings::<see cref="NailBinding"/></c>
+        /// </summary>
+        public string ID { get; }
+
+        /// <summary>
+        /// Gets the display name of this binding.
         /// </summary>
         public string Name { get; }
 
         /// <summary>
         /// Gets whether this binding is currently applied.
         /// </summary>
-        [JsonIgnore]
         public bool IsApplied { get; private set; }
 
         /// <summary>
         /// Gets whether this binding is a base game binding.
         /// </summary>
-        [JsonIgnore]
         public bool IsVanillaBinding { get; }
 
         /// <summary>
@@ -42,7 +58,7 @@ namespace ToggleableBindings
         /// <see cref="OnApplied"/> accordingly.
         /// This property is set when the binding is serialized.
         /// </summary>
-        [JsonProperty]
+        [JsonProperty(Order = -5)]
         protected internal bool WasApplied { get; protected set; }
 
         /// <summary>
@@ -54,8 +70,12 @@ namespace ToggleableBindings
             if (name is null)
                 throw new ArgumentNullException(nameof(name));
 
+            Type bindingType = GetType();
+            string asmName = bindingType.Assembly.GetName().Name;
+
+            ID = $"{asmName}::{bindingType.Name}";
             Name = name;
-            if (IsVanilla(GetType())) // Derived class has VanillaBindingAttribute applied to it.
+            if (IsVanilla(bindingType)) // Vanilla bindings have VanillaBindingAttribute applied to them.
                 IsVanillaBinding = true;
         }
 
@@ -115,6 +135,10 @@ namespace ToggleableBindings
 
         /// <summary>
         /// Applies this binding, enabling its effects.
+        /// <para>
+        /// Note: Bindings should be registered with <see cref="BindingManager"/> before
+        /// being applied or restored.
+        /// </para>
         /// </summary>
         public void Apply()
         {
@@ -128,6 +152,7 @@ namespace ToggleableBindings
 
         /// <summary>
         /// Restores this binding, disabling its effects.
+        /// <para><inheritdoc cref="Apply"/></para>
         /// </summary>
         public void Restore()
         {
@@ -140,14 +165,19 @@ namespace ToggleableBindings
         }
 
         /// <summary>
-        /// Executed when <see cref="Apply"/> is called only if the binding was not already applied.
+        /// Executed when <see cref="Apply"/> is called only if the binding is not applied.
         /// <see cref="Applied"/> is invoked after this method is called.
         /// </summary>
         protected abstract void OnApplied();
 
         /// <summary>
-        /// Executed when <see cref="Restore"/> is called only if the binding was previously applied.
+        /// Executed when <see cref="Restore"/> is called only if the binding is applied.
         /// <see cref="Restored"/> is invoked after this method is called.
+        /// <para>
+        /// Note: This method should be guaranteed to put the object in a 'fresh' state (as in, just initialized).
+        /// <br/>
+        /// Failure to do so may result in unexpected behavior when moving between save files.
+        /// </para>
         /// </summary>
         protected abstract void OnRestored();
 
@@ -163,7 +193,7 @@ namespace ToggleableBindings
             WasApplied = false;
         }
 
-        internal static bool IsVanilla(Type bindingType)
+        private static bool IsVanilla(Type bindingType)
         {
             return bindingType.IsDefined(typeof(VanillaBindingAttribute), false);
         }
