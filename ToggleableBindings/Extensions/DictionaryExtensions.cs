eb5606d383147a166ebeadccd3dbb05c7fd5236c
Unordinal <unordinal@gmail.com>
2021-02-26 12:35:06 -0800

    Uses less specific type.

--- ToggleableBindings/Extensions/DictionaryExtensions.cs
+++ ToggleableBindings/Extensions/DictionaryExtensions.cs
@@ -6,7 +6,7 @@ namespace ToggleableBindings.Extensions
 {
     public static class DictionaryExtensions
     {
-        public static TValue? GetValueOrDefault<TKey, TValue>(this Dictionary<TKey, TValue> dictionary, TKey key, TValue? defaultValue = default)
+        public static TValue? GetValueOrDefault<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, TValue? defaultValue = default)
         {
             return dictionary.TryGetValue(key, out var value) ? value : defaultValue;
         }
