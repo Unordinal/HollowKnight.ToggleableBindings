aee89883b5c9d48939427422fd13d50b3ec2f6f5
Unordinal <unordinal@gmail.com>
2021-03-02 16:05:31 -0800

    Enable nullable.

--- ToggleableBindings/JsonNet/ActionComboJsonConverter.cs
+++ ToggleableBindings/JsonNet/ActionComboJsonConverter.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿#nullable enable
+
+using System;
 using Newtonsoft.Json;
 using ToggleableBindings.Input;
 
@@ -11,7 +13,7 @@ namespace ToggleableBindings.JsonNet
             return objectType == typeof(ActionCombo);
         }
 
-        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        public override object? ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
         {
             if (objectType != typeof(ActionCombo))
                 throw new JsonException("Object is not ActionCombo.");
@@ -19,7 +21,7 @@ namespace ToggleableBindings.JsonNet
             return Keybinds.KeybindStringToCombo(reader.Value as string);
         }
 
-        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        public override void WriteJson(JsonWriter writer, object? value, JsonSerializer serializer)
         {
             if (value is not ActionCombo actionCombo)
                 throw new JsonException("Object is not ActionCombo.");
